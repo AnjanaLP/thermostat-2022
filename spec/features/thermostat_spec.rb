c5c84264a8228c977d0c388f214b295b67bb913a
AnjanaLP <anjana.lpn@gmail.com>
2022-01-27 20:18:28 +0000

    add feature tests for viewing the temperature

--- spec/features/thermostat_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'View temperature' do
-  scenario 'see the default starting temperature' do
-    visit '/'
-    expect(page).to have_content "20"
-  end
-end
---
+++ spec/features/viewing_temperature_spec.rb
@@ -0,0 +1,35 @@
+feature 'Viewing temperature' do
+  before do
+    visit '/'
+  end
+
+  scenario 'initially see the default temperature' do
+    expect(page).to have_content "20"
+  end
+
+  context 'when the temperature is increased' do
+    scenario 'see the change up' do
+      page.find('#temperature-up').click
+      expect(page.find('#temperature')).to have_content '21'
+      expect(page.find('#temperature')).not_to have_content '20'
+    end
+  end
+
+  context 'when the temperature is decreased' do
+    scenario 'see the change down' do
+      page.find('#temperature-down').click
+      expect(page.find('#temperature')).to have_content '19'
+      expect(page.find('#temperature')).not_to have_content '20'
+    end
+  end
+
+  context 'when the temperature is reset' do
+    scenario 'see the default temperature' do
+      5.times { page.find('#temperature-up').click }
+      expect(page.find('#temperature')).to have_content '25'
+      page.find('#temperature-reset').click
+      expect(page.find('#temperature')).to have_content '20'
+      expect(page.find('#temperature')).not_to have_content '25'
+    end
+  end
+end
